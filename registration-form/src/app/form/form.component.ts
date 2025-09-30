237c6c88850328237d66d6d82ec2b496522e6885
sakshi <theurkarsakshi0909@gmail.com>
2025-09-30 22:01:25 +0530

    first commit

---
+++ registration-form/src/app/form/form.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+Submit(form:any){
+  console.log(form);
+  alert("Form is Successfully Submitted");
+  
+}
+courses:string[]=[
+'web Development',
+'Data Science',
+'Artifical intelligence'
+]
+
+}
